364c4ad9421b69d7414da787154a93d842c8666f
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-08-13 14:18:03 +0300

    Update form-country-input.js
    Update API and switch to vanilla JS from JQuery.

--- form-country-input.js
+++ form-country-input.js
@@ -1,21 +1,29 @@
 const mobileInput = document.querySelector("#Mobile");
 const countryInput = document.querySelector("#Country");
 const iti = window.intlTelInput(mobileInput, {
-    autoHideDialCode: false,
+    initialCountry: "auto",
     autoPlaceholder: "on",
     geoIpLookup: function (callback) {
-        // TODO use vanilla JS instead!
-        $.get("https://api.ipgeolocation.io/ipgeo?apiKey=422275249a7e4de6ab10a264a99114f8&fields=geo", function () {
-        }, "json").always(function (resp) {
-            const countryCode = (resp && resp.country_code2) ? resp.country_code2 : "";
-            callback(countryCode);
-        });
+        // TODO medium security issue - publicly visible api key here!
+        fetch("https://api.ipgeolocation.io/ipgeo?apiKey=422275249a7e4de6ab10a264a99114f8&fields=geo")
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (data) {
+                callback(data?.country_code2 ?? "us");
+            })
+            .catch(function (reason) {
+                callback("us");
+                console.error(reason)
+            });
     },
-    initialCountry: "auto",
+
     nationalMode: false,
-    preferredCountries: ['gb', 'tr', 'fr', 'sa', 'us', 'de', 'se', 'be', 'kw', 'ae', 'qa', 'nl'],
+    autoInsertDialCode: true,
+    autoHideDialCode: false,
     separateDialCode: false,
-    utilsScript: "https://lp.dentfixturkey.com/veneer-turkey/assets/js/utils.js",
+    preferredCountries: ['gb', 'tr', 'fr', 'sa', 'us', 'de', 'se', 'be', 'kw', 'ae', 'qa', 'nl'],
+    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
 });
 
 mobileInput.addEventListener('countrychange', function (e) {
