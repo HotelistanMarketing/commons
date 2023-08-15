7c63c170d6268f63a2480fa4bf0aa8003b8fbd2e
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-08-15 14:45:50 +0300

    Create optimize-imgs.php

---
+++ devtools/optimize-imgs.php
@@ -0,0 +1,56 @@
+<?php
+
+$img_dir = defined('IMG_DIR') ? constant('IMG_DIR') : getcwd() . '/assets';
+$formats = defined('IMG_OPT_FORMATS') ? constant('IMG_OPT_FORMATS') : ['png', 'jpeg', 'jpg'];
+$quality = defined('IMG_OPT_QUALITY') ? constant('IMG_OPT_QUALITY') : 100;
+$excl_re = defined('IMG_OPT_EXCLUDE_RE') ? constant('IMG_OPT_EXCLUDE_RE') : '/^ico-/';
+/**
+ * @throws Exception if gd module is not enabled.
+ */
+function convertToWebp($source, $quality = 100, $removeOld = false)
+{
+    if (!extension_loaded('gd'))
+        throw new Exception('Please enable gd first!');
+
+    $dir = pathinfo($source, PATHINFO_DIRNAME);
+    $name = pathinfo($source, PATHINFO_FILENAME);
+    $destination = $dir . DIRECTORY_SEPARATOR . $name . '.webp';
+    $info = getimagesize($source);
+    $isAlpha = false;
+
+    if ($info['mime'] == 'image/jpeg')
+        $image = imagecreatefromjpeg($source);
+    elseif ($isAlpha = $info['mime'] == 'image/gif')
+        $image = imagecreatefromgif($source);
+    elseif ($isAlpha = $info['mime'] == 'image/png')
+        $image = imagecreatefrompng($source);
+    else
+        return $source;
+
+    if ($isAlpha) { // gd does not create alpha channel by default!
+        imagepalettetotruecolor($image);
+        imagealphablending($image, true);
+        imagesavealpha($image, true);
+    }
+
+    imagewebp($image, $destination, $quality);
+
+    if ($removeOld)
+        unlink($source);
+
+    return $destination;
+}
+
+$it = new RecursiveDirectoryIterator($img_dir);
+foreach (new RecursiveIteratorIterator($it) as $file) {
+    if (in_array($file->getExtension(), $formats)) {
+        if (preg_match($excl_re, pathinfo($file, PATHINFO_FILENAME)))
+            continue;
+
+        try {
+            echo 'Optimizing ' . $file . '...' . PHP_EOL;
+            convertToWebp($file, quality: $quality);
+        } catch (Exception $e) {
+        }
+    }
+}
