93aee8b67cbb8a818c326429e0e1365f7479bd4e
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-08-23 16:30:49 +0300

    Update form-validation.php

--- form-validation.php
+++ form-validation.php
@@ -1,31 +1,23 @@
 <script>
-    function formFieldCheck() {
-        const mndFields = ['Last Name', 'Mobile'];
-        const fldLangVal = ['Name\x20\x26\x20Surname', 'Mobile'];
+    function formValidation() {
+        const mndFields = ['Last_Name', 'Mobile'];
         for (let i = 0; i < mndFields.length; i++) {
             const fieldObj = document.forms.namedItem("contactForm")[mndFields[i]];
             if (fieldObj) {
                 if (((fieldObj.value).replace(/^\s+|\s+$/g, '')).length === 0) {
-                    alert("<?= TR['form_empty_field_warning'] ?>".replace("%s", fldLangVal[i]));
                     fieldObj.focus();
+                    alert("<?= TR['form_empty_field_warning'] ?>".replace("%s", fieldObj.placeholder));
                     return false;
                 }
-                else if (fieldObj.name === "Mobile") {
+                else if (fieldObj.id === "Mobile") {
                     // TODO improve by checking alphanumeric!
                     if (fieldObj.value.length < 8 || fieldObj.value.length > 15) {
-                        alert("<?= TR['form_phone_validation'] ?>")
                         fieldObj.focus();
+                        alert("<?= TR['form_phone_validation'] ?>")
                         return false;
                     }
                 }
-                try {
-                    if (fieldObj.name === 'Last Name')
-                        name = fieldObj.value;
-                } catch (e) {
-                    console.error(e)
-                }
             }
         }
-        document.querySelector('.crmWebToEntityForm .formsubmit').setAttribute('disabled', true);
     }
 </script>
