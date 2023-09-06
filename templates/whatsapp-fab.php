11faf390b2a6095f0364e686d688d18aaa82b537
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-09-06 21:52:41 +0300

    Add whatsapp-fab.php

---
+++ templates/whatsapp-fab.php
@@ -0,0 +1,16 @@
+<?php
+
+/* TODO improve WhatsApp FAB!
+ * Use a svg icon instead!
+ */
+
+?>
+
+<a id="whatsapp-fab" href="<?= WHATSAPP_LINK ?>" target="_blank">
+    <img class="wp-icon" src="/assets/ico-wp.png" width="130" height="91" alt="WhatsApp">
+    <div class="wp-dots">
+        <div class="dot one"></div>
+        <div class="dot two"></div>
+        <div class="dot three"></div>
+    </div>
+</a>
