944f6d94afaefb17cc409abcfa0e7a258ff1b4a2
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-09-17 11:31:16 +0300

    Cleanup

--- functions.php
+++ functions.php
@@ -38,9 +38,11 @@ function get_page_style($file_name): string
  */
 function get_google_font(string $name, string $weights = '400;700', string $display = 'block'): void
 {
+    // @formatter:off
     ?>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=<?= $name ?>:wght@<?= $weights ?>&display=<?= $display ?>">
     <?php
+    // @formatter:on
 }
