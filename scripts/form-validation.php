656c4342fb27f76b13e9997d201e568554243f3e
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-09-22 12:48:25 +0300

    Customizable form validation

--- scripts/form-validation.php
+++ scripts/form-validation.php
@@ -1,18 +1,25 @@
 <script>
-    function formValidation() {
+    function formValidation(nameRequired = true, alert = true) {
         const form = document.forms.namedItem("contactForm");
-        const nameInput = form['Last_Name'];
-        if (nameInput && nameInput.value.replace(/^\s+|\s+$/g, '').length === 0) {
-            alert("<?= TR['form_empty_field_warning'] ?>".replace("%s", nameInput.placeholder));
-            nameInput.focus();
-            return false;
+
+        if (nameRequired) {
+            const nameInput = form['Last_Name'];
+            if (nameInput && nameInput.value.replace(/^\s+|\s+$/g, '').length === 0) {
+                if (alert)
+                    alert("<?= TR['form_empty_field_warning'] ?>".replace("%s", nameInput.placeholder));
+
+                nameInput.focus();
+                return false;
+            }
         }
 
         const input = form['Phone'];
         const iti = window.intlTelInputGlobals.getInstance(input);
         const isPhoneNumberValid = iti.isValidNumber();
         if (!isPhoneNumberValid) {
-            alert("<?= TR['form_phone_validation'] ?>")
+            if (alert)
+                alert("<?= TR['form_phone_validation'] ?>")
+
             input.focus();
         }
 
