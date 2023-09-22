8eeffb841a45fc933831f28e5753927f776e94c6
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-09-22 17:48:24 +0300

    Revert "Customizable form validation"
    This reverts commit 656c4342fb27f76b13e9997d201e568554243f3e.

--- scripts/form-validation.php
+++ scripts/form-validation.php
@@ -1,25 +1,18 @@
 <script>
-    function formValidation(nameRequired = true, alert = true) {
+    function formValidation() {
         const form = document.forms.namedItem("contactForm");
-
-        if (nameRequired) {
-            const nameInput = form['Last_Name'];
-            if (nameInput && nameInput.value.replace(/^\s+|\s+$/g, '').length === 0) {
-                if (alert)
-                    alert("<?= TR['form_empty_field_warning'] ?>".replace("%s", nameInput.placeholder));
-
-                nameInput.focus();
-                return false;
-            }
+        const nameInput = form['Last_Name'];
+        if (nameInput && nameInput.value.replace(/^\s+|\s+$/g, '').length === 0) {
+            alert("<?= TR['form_empty_field_warning'] ?>".replace("%s", nameInput.placeholder));
+            nameInput.focus();
+            return false;
         }
 
         const input = form['Phone'];
         const iti = window.intlTelInputGlobals.getInstance(input);
         const isPhoneNumberValid = iti.isValidNumber();
         if (!isPhoneNumberValid) {
-            if (alert)
-                alert("<?= TR['form_phone_validation'] ?>")
-
+            alert("<?= TR['form_phone_validation'] ?>")
             input.focus();
         }
 
