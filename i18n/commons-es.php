ef4a6e662a68ebf2879c617195870d04511bde24
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-09-30 17:19:13 +0300

    Add Spanish localization

---
+++ i18n/commons-es.php
@@ -0,0 +1,16 @@
+<?php
+
+const TRC = array(
+    // FORM
+    "form_name" => "Nombre y Apellidos",
+    "form_name_opt" => "Nombre (opcional)",
+    "form_mail" => "Correo electrónico (opcional)",
+    "form_phone" => "Teléfono",
+    "form_empty_field_warning" => "%s no puede estar vacío.",
+    "form_phone_validation" => "Por favor introduzca un número de teléfono válido.",
+
+    // WP FORM
+    "wp_form_title" => "Enviar mensaje de WhatsApp",
+    "wp_form_description" => "Rellena el formulario y serás redirigido a WhatsApp.",
+    "wp_form_button" => "Continuar con WhatsApp",
+);
