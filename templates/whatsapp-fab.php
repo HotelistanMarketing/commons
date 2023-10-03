576146703483538796adadd13d926c5bd4a0599f
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-10-03 17:10:22 +0300

    Fix last commit

--- templates/whatsapp-fab.php
+++ templates/whatsapp-fab.php
@@ -1,4 +1,4 @@
 <a id="whatsapp-fab" href="<?= WHATSAPP_LINK ?>" target="_blank">
     <img src="/commons/assets/wp-white.png" alt="WhatsApp" width="1500" height="1508">
-    <span><?= TR['whatsapp_button'] ?></span>
+    <span><?= TRC['wp_fab'] ?></span>
 </a>
