8082b9f5e7c6c2245707c469c4a307c04cf23bbb
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-10-20 15:08:38 +0300

    Add Italian localization

---
+++ i18n/commons-it.php
@@ -0,0 +1,19 @@
+<?php
+
+const TRC = array(
+    // FORM
+    "form_name" => "Nome e cognome",
+    "form_name_opt" => "Nome (opzionale)",
+    "form_mail" => "E-Mail (opzionale)",
+    "form_phone" => "Numero di telefono",
+    "form_empty_field_warning" => "%s Il campo non può essere vuoto.",
+    "form_phone_validation" => "Per favore, inserisci un numero di telefono valido.",
+
+    // WP FAB
+    "wp_fab" => "Contattaci",
+
+    // WP FORM
+    "wp_form_title" => "Invia un messaggio WhatsApp",
+    "wp_form_description" => "Compila il modulo e sarai reindirizzato su WhatsApp.",
+    "wp_form_button" => "Continua su WhatsApp",
+);
