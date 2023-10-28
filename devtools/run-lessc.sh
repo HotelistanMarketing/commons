fbb12c2eb471b18a642020f8c896beabdbb95913
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-10-29 00:59:45 +0300

    Improve run-lessc.sh script: - Check variants dir as well - Check if files exist

--- devtools/run-lessc.sh
+++ devtools/run-lessc.sh
@@ -5,7 +5,12 @@ PATH=/workspace/node_modules/.bin:$PATH
 # Add node executable to the path since lessc cannot find it.
 PATH=/layers/heroku_nodejs-engine/dist/bin:$PATH
 
-for file in /workspace/pages/*/*.less
+dirs=(/workspace/pages/*/*.less /workspace/variants/*/*.less)
+for file in "${dirs[@]}"
 do
-  lessc "$file" "${file/.less/.css}" --clean-css="--s0 --advanced"
+  if test -f "$file"
+  then
+    echo "... Compiling $file"
+    lessc "$file" "${file/.less/.css}" --clean-css="--s0 --advanced"
+  fi
 done
