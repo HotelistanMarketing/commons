204cc8162281106e9b94618a44f9b6bc370fe31c
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-11-27 22:58:08 +0300

    Don't include analytic scripts on dev envs

--- scripts/gtm.php
+++ scripts/gtm.php
@@ -4,10 +4,15 @@
  * Requires GTM_ID constant!
  */
 ?>
-<script>
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','<?= GTM_ID ?>');
-</script>
+
+<?php if (PRODUCTION): ?>
+    <script>
+        // @formatter:off
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','<?= GTM_ID ?>');
+        // @formatter:on
+    </script>
+<?php endif ?>
