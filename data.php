e0c8073eb90d2d14f7aad1242bc8cb358eb825ec
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-12-30 10:54:32 +0300

    Add lead data consts & autoloader via composer

---
+++ data.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Lead\Language {
+    const Arabic = 'Arabic';
+    const English = 'English';
+    const French = 'French';
+    const German = 'German';
+    const Italian = 'İtalian';
+    const Spanish = 'Spanish';
+    const Swedish = 'Swedish';
+    const Turkish = 'Türkçe';
+}
+
+namespace Lead\Interest {
+    const Aesthetics = 'Aesthetics';
+    const Dental = 'Dental';
+}
+
+namespace Lead\Procedure {
+    const BBL = 'BBL';
+    const BreastAugmentations = 'Breast Augmentations';
+    const Implant = 'Implant';
+    const Liposuction = 'Liposuction';
+    const Rhinoplasty = 'Rhinoplasty';
+    const TummyTuck = 'Tummy Tuck';
+}
