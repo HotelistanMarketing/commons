9375515ed0d237395b1138c2605d03164df9a3ae
mozanyazar <mahmutozanyazar@gmail.com>
2024-01-12 17:42:45 +0300

    add: abandon lead validation

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -1,43 +1,82 @@
-document.querySelectorAll('form').forEach(form => {
-    const nameInput = form['Last_Name']
-    const mobileInput = form['Phone']
+var iti
 
-    nameInput.addEventListener('input', event => {
-        if (event.target.value.trim().length === 0)
-            event.target.value = ""
-    })
+const getWebsite = () => {
+     const location = window.location.href
+     return location.split('?')[0]
+}
 
-    mobileInput.addEventListener('input', event => event.target.setCustomValidity(''))
+document.querySelectorAll('form').forEach((form) => {
+     const nameInput = form['Last_Name']
+     const mobileInput = form['Phone']
+     const emailInput = form['Email']
+     const website = getWebsite()
 
-    form.addEventListener('submit', event => {
-        if (!formValidation(event.target))
-            event.preventDefault()
-    })
+     iti = window.intlTelInputGlobals.getInstance(mobileInput)
+
+     nameInput.addEventListener('input', (event) => {
+          if (event.target.value.trim().length === 0) event.target.value = ''
+     })
+
+     // email change handler
+     //  emailInput.addEventListener('input', async (event) => {
+     //       const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+     //       if (event.target.value.match(validRegex) && iti.isValidNumber()) {
+     //            const validNumber = iti.getNumber()
+
+     //            await fetch(
+     //                 `https://hotelistan-services.freeddns.org/api/service/abandon?phone=${validNumber}&website=${website}&name=${nameInput.value}&email=${event.target.value}`
+     //            )
+     //       }
+     //  })
+
+     // phone change handler
+     mobileInput.addEventListener('input', async (event) => {
+          event.target.setCustomValidity('')
+          const isPhoneNumberValid = iti.isValidNumber()
+
+          if (isPhoneNumberValid) {
+               const phoneNumber = iti.getNumber()
+               await fetch(
+                    `https://hotelistan-services.freeddns.org/api/service/abandon?phone=${phoneNumber}&website=${website}&name=${nameInput.value}&email=${emailInput.value}`,
+                    { credentials: 'include' }
+               )
+          }
+     })
+
+     form.addEventListener('submit', (event) => {
+          if (!formValidation(event.target)) event.preventDefault()
+     })
 })
 
 /**
  * @returns boolean whether all fields are valid (true) or not (false)
  */
-function formValidation(form) {
-    const mobileInput = form['Phone']
-    const iti = window.intlTelInputGlobals.getInstance(mobileInput)
-    const isPhoneNumberValid = iti.isValidNumber()
+async function formValidation(form) {
+     const mobileInput = form['Phone']
+     const isPhoneNumberValid = iti.isValidNumber()
 
-    if (!isPhoneNumberValid) {
-        mobileInput.setCustomValidity(mobileInput.getAttribute('data-warning-msg'))
-        mobileInput.reportValidity()
-    }
-    else {
-        const nameInput = form['Last_Name']
-        nameInput.value = nameInput.value.trim()
-        if (nameInput.value.length === 0 && !nameInput.required) {
-            // Empty values are displayed as 'Not Provided' on Zoho, but...
-            // The King of the Lead Managers, Mr. Alperen, requested us to make it 'Unknown'.
-            // So, submit empty value as 'Unknown' without showing it to user:
-            nameInput.style.color = "transparent"
-            nameInput.value = "Unknown"
-        }
-    }
+     if (!isPhoneNumberValid) {
+          mobileInput.setCustomValidity(mobileInput.getAttribute('data-warning-msg'))
+          mobileInput.reportValidity()
+     } else {
+          const nameInput = form['Last_Name']
+          nameInput.value = nameInput.value.trim()
 
-    return isPhoneNumberValid
+          if (nameInput.value.length === 0 && !nameInput.required) {
+               // Empty values are displayed as 'Not Provided' on Zoho, but...
+               // The King of the Lead Managers, Mr. Alperen, requested us to make it 'Unknown'.
+               // So, submit empty value as 'Unknown' without showing it to user:
+               nameInput.style.color = 'transparent'
+               nameInput.value = 'Unknown'
+          }
+     }
+
+     const validNumber = iti.getNumber()
+
+     await fetch(`https://hotelistan-services.freeddns.org/api/service/abandon/del?phone=${validNumber}`, {
+          credentials: 'include',
+     })
+
+     return isPhoneNumberValid
 }
