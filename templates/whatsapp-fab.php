ee48c26e293e06efb7ca104655397cc1f5d2686b
Mutlu Kasap <mutlukasapweb@gmail.com>
2025-09-13 15:27:01 +0300

    update: changed wp style.

--- templates/whatsapp-fab.php
+++ templates/whatsapp-fab.php
@@ -6,7 +6,8 @@ assert(defined('WHATSAPP_LINK'));
 // Here we removed target="_blank" to work around a bug,
 // which was preventing WhatsApp from opening via Chrome on iOS.
 ?>
-<a id="whatsapp-fab" class="collapsed" href="<?= WHATSAPP_LINK ?>">
-    <img src="/commons/assets/wp-white.webp" alt="WhatsApp" width="128" height="129">
-    <span><?= TRC['wp_fab'] ?></span>
-</a>
+<div class="btn-shadow">
+    <a id="whatsapp-fab" class="collapsed" href="<?= WHATSAPP_LINK ?>">
+        <img src="/commons/assets/wp-white.webp" alt="WhatsApp" width="128" height="129">
+    </a>
+</div>
