db355076d6077f159942a0e9d31c6844b473a66f
Utku Bayguven <utkubayguven@distedavim.com>
2025-10-25 08:28:33 +0300

    Remove deprecated Telegram and WhatsApp floating action button scripts and styles, along with associated HTML templates. Clean up accessibility and dimensions styles, and eliminate unused form and meta templates. This refactor aims to streamline the codebase by removing unnecessary components and improving maintainability.

--- templates/telegram-fab.php
+++
@@ -1,7 +0,0 @@
-<?php
-assert(defined('TELEGRAM_LINK'));
-?>
-
-<a id="telegram-fab" class="collapsed" href="<?= TELEGRAM_LINK ?>">
-    <img src="/commons/assets/tg-white.webp" alt="Telegram" width="128" height="129">
-</a>
